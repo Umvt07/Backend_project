bad1fbddc1a29980a21e7d2232bc1338c24bd6a4
Adarsh Kumar <sharmaadarsh10534@gmail.con>
2026-06-19 19:44:48 +0530

    Connected to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,39 @@
+// require('dotenv').config({path:'./env'})
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+dotenv.config({
+    path:"./env",
+})
+
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+// ;( async ()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//         app.on("error",(error)=>{
+//             console.log("ERRRR",error);
+//             throw error
+//         })
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`APP is listining on port ${process.env.PORT}`);
+//         })
+//     } catch (error) {
+//         console.log("ERROR",error);
+//         throw err;
+//     }
+// })()  // semi colon because it flushes the above thing
